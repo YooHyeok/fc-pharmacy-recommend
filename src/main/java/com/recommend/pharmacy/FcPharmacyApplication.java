e081e284914039d1af69b2facd697d8edfc98995
webdevyoo <yjk7454@naver.com>
2024-04-16 11:30:38 +0900

    Initial commit

---
+++ src/main/java/com/recommend/pharmacy/FcPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.recommend.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcPharmacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recommend/pharmacy/FcPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommend.pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcPharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
