368d485adbb787148d7c9e76a71318b87436bf53
webdevyoo <yjk7454@naver.com>
2024-04-20 00:32:06 +0900

    Ch05-10.[Transaction]: Self Invocation 트랜잭션 예외 테스트 케이스 구현

--- src/test/groovy/com/recommend/pharmacy/service/PharmacyRepositoryServiceTest.groovy
+++ src/test/groovy/com/recommend/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -59,4 +59,28 @@ class PharmacyRepositoryServiceTest extends AbstractIntergaionContainerBaseTest 
         then:
         result.get(0).getPharmacyAddress() == modifiedAddress
     }
+
+    def "self invocation"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build();
+
+        when:
+        pharmacyRepositoryService.bar(List.of(pharmacy))
+
+        then:
+        def e = thrown(RuntimeException.class) // thrown() 메소드를 통해 RuntimeException 발생 여부 확인
+        println e.message
+        def result = pharmacyRepositoryService.findAll()
+        result.size() == 1 // 롤백이 됬을 경우 size는 0이여야 하지만 실제로는 1임.. (트랜잭션 정상적용안됨)
+    }
 }
