3deff0517735496ba955549e2ce29818b0d7344e
webdevyoo <yjk7454@naver.com>
2024-04-19 10:27:23 +0900

    Ch05.09.[JPA Auditing]: Auditing - BaseEntity 추상클래스 구현 (작성일자,수정일자)

---
+++ src/main/java/com/recommend/pharmacy/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.recommend.pharmacy;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false) // 수정되지 말아야할 정보
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifieDate;
+
+}
