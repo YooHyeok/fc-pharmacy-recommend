f9f6bac3df29742b7a8d16b1cbd09bc57837658b
webdevyoo <yjk7454@naver.com>
2024-04-22 10:15:25 +0900

    Ch.05-11.[초기데이터셋업.v2]: PharmacyDto 클래스 추가

---
+++ src/main/java/com/recommend/pharmacy/dto/PharmacyDto.java
@@ -0,0 +1,15 @@
+package com.recommend.pharmacy.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PharmacyDto {
+
+    private Long id;
+    private String pharmacyName;
+    private String pharmacyAddress;
+    private double latitude;
+    private double longitude;
+}
