8785c8496c640368b52e2f172db5d179ee062ef1
webdevyoo <yjk7454@naver.com>
2024-04-22 10:24:59 +0900

    Ch.05-11.[초기데이터셋업.v2]: 약국 API CSV 변환 DTO 리스트 MariaDB 저장 API 기능 구현

---
+++ src/main/java/com/recommend/pharmacy/controller/PharmacyController.java
@@ -0,0 +1,40 @@
+package com.recommend.pharmacy.controller;
+
+import com.recommend.pharmacy.api.entity.Pharmacy;
+import com.recommend.pharmacy.service.PharmacyRepositoryService;
+import com.recommend.pharmacy.util.CsvUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class PharmacyController {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    @GetMapping("/csv/save")
+    public String saveCsv() {
+        saveCsvToDatabase(); // MariaDB 에 저장
+        return "success save";
+    }
+
+    public void saveCsvToDatabase() {
+        List<Pharmacy> pharmacyDtoList = CsvUtils.convertToPharmacyDtoList()
+                .stream().map(pharmacyDto ->
+                        Pharmacy.builder()
+                                .id(pharmacyDto.getId())
+                                .pharmacyName(pharmacyDto.getPharmacyName())
+                                .pharmacyAddress(pharmacyDto.getPharmacyAddress())
+                                .latitude(pharmacyDto.getLatitude())
+                                .longitude(pharmacyDto.getLongitude())
+                                .build()
+                ).collect(Collectors.toList());
+        pharmacyRepositoryService.saveAll(pharmacyDtoList);
+    }
+}
