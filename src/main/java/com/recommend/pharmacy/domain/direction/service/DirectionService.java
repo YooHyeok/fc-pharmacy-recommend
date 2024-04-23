faed9a11d2da5d1fe441acddae3b0b61760d54fd
webdevyoo <yjk7454@naver.com>
2024-04-23 09:17:58 +0900

    Ch.05-12.[거리계산]: 검색한 고객주소 기준 반경 10KM 내 최대 3개 약국 조회 기능 구현

--- src/main/java/com/recommend/pharmacy/domain/direction/service/DirectionService.java
+++ src/main/java/com/recommend/pharmacy/domain/direction/service/DirectionService.java
@@ -9,12 +9,62 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DirectionService {
+
+    private static final int MAX_SEARCH_COUNT = 3;// 약국 최대 검색 갯수
+    private static final double RADIUS_KM = 10.0;// 반경 10 KM
+    private final PharmacySearchService pharmacySearchService;
+
+    /**
+     * 최대 3개의 약국 데이터
+     * Kakao API를 통해 고객이 입력한 문자열 기반 주소 정보를 통해 위치기반 데이터 위/경도로 반환하여
+     * DB에 저장된 데이터를 수집한뒤, 거리계산 알고리즘을 적용하여 고객과 약국 사이의 거리를 계산하고 정렬한다.
+     * 해당 반환값은 List형태로 담겨있다.
+     */
+    public List<Direction> buildDirectionList(DocumentDto documentDto) {
+
+        /* 데이터가 null인 경우에 대한 유효성 검증 (입력한 주소가 존재하지 않을 경우) */
+        if(Objects.isNull(documentDto)) return Collections.emptyList();
+
+        // 약국데이터 조회
+        List<PharmacyDto> pharmacyDtos = pharmacySearchService.searchPharmacyDtoList();
+
+        //거리 계산 알고리즘을 이용하여, 고객과 약국 사이의 거리를 계산하고 sort
+        return pharmacyDtos.stream().map(pharmacyDto ->
+                        Direction.builder()
+                                /* 고객이 입력한 정보(고객위치) */
+                                .inputAddress(documentDto.getAddressName())
+                                .inputLatitude(documentDto.getLatitude())
+                                .inputLongitude(documentDto.getLongitude())
+                                /* 약국의 정보 */
+                                .targetPharmacyName(pharmacyDto.getPharmacyName())
+                                .targetAddress(pharmacyDto.getPharmacyAddress())
+                                .targetLatitude(pharmacyDto.getLatitude())
+                                .targetLongitude(pharmacyDto.getLongitude())
+                                /* 고객-약국간 거리 (하버사인포뮬러) */
+                                .distance(
+                                        calcutateDistance(documentDto.getLatitude(), documentDto.getLongitude(),
+                                                pharmacyDto.getLatitude(), pharmacyDto.getLongitude()
+                                        )
+                                ).build()
+                )
+                /* 반경 10KM 이내에 있는 Direction만 필터링 */
+                .filter(direction -> direction.getDistance() <= RADIUS_KM)
+                /* 거리 기준 오름차순 정렬 */
+                .sorted(Comparator.comparing(Direction::getDistance))
+                /* 최대 3개까지만 */
+                .limit(MAX_SEARCH_COUNT)
+                .collect(Collectors.toList());
+    }
 
     /**
      * 두 위/경도 간의 거리를 구한다. <br/>
@@ -29,7 +79,5 @@ public class DirectionService {
         lon2 = Math.toRadians(lon2);
         double earthRadius = 6371; //Kilometers
         return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
-
-        )
     }
 }
