f71a16f3e83aae11caed1a605462cd6da866e06a
webdevyoo <yjk7454@naver.com>
2024-04-23 09:17:20 +0900

    Ch.05-12.[거리계산]: 고객 위치정보 위/경도 필드 String → double 타입 수정

--- src/main/java/com/recommend/pharmacy/api/dto/DocumentDto.java
+++ src/main/java/com/recommend/pharmacy/api/dto/DocumentDto.java
@@ -1,12 +1,10 @@
 package com.recommend.pharmacy.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -15,9 +13,9 @@ public class DocumentDto {
     private String addressName; // 검색어로 사용된 주소값
 
     @JsonProperty("y")
-    private String latitude; // 위도(y)
+    private double latitude; // 위도(y)
 
     @JsonProperty("x")
-    private String longitude; // 경도(x)
+    private double longitude; // 경도(x)
 
 }
