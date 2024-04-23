7506886cce189a8f0075d64db0c2239b20aa1837
webdevyoo <yjk7454@naver.com>
2024-04-23 09:16:00 +0900

    Ch.05-12.[거리계산]: 거리계산시 사용할 Direction Entity 클래스 추가

---
+++ src/main/java/com/recommend/pharmacy/domain/direction/entity/Direction.java
@@ -0,0 +1,37 @@
+package com.recommend.pharmacy.domain.direction.entity;
+
+import com.recommend.pharmacy.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "direction")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Direction extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /* 고객 주소 정보 */
+    private String inputAddress;
+    private double inputLatitude;
+    private double inputLongitude;
+
+    /* 약국 주소 정보 */
+    private String targetPharmacyName;
+    private String targetAddress;
+    private double targetLatitude;
+    private double targetLongitude;
+
+    private double distance;
+
+}
