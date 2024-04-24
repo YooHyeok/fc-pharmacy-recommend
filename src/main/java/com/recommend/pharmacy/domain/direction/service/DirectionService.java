255eb9b31f88b8e682763d973a8ff01c36066b1d
webdevyoo <yjk7454@naver.com>
2024-04-25 00:05:31 +0900

    Ch.05-17[카테고리장소검색API]: 카테고리 검색 API를 활용한 반경 10KM내 가장 가까운 약국 3개 목록조회 구현

--- src/main/java/com/recommend/pharmacy/domain/direction/service/DirectionService.java
+++ src/main/java/com/recommend/pharmacy/domain/direction/service/DirectionService.java
@@ -1,6 +1,7 @@
 package com.recommend.pharmacy.domain.direction.service;
 
 import com.recommend.pharmacy.api.dto.DocumentDto;
+import com.recommend.pharmacy.api.service.KakaoCategorySearchService;
 import com.recommend.pharmacy.domain.direction.entity.Direction;
 import com.recommend.pharmacy.domain.direction.repository.DirectionRepository;
 import com.recommend.pharmacy.domain.pharmacy.dto.PharmacyDto;
@@ -18,6 +19,18 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+/**
+ * - 카카오 주소API를 통해 문자열 기반 주소 정보중 가장 첫 데이터의 위치기반 데이터인 위,경도로 변환
+ *
+ * [buildDirectionList]
+ * 	- 거리계산 알고리즘 적용으로 목록 추출 - `pharmacySearchService.searchPharmacyDtoList()`
+ * 		- DB에 저장된 약국 데이터 수집
+ * 		- 고객 약국사이 거리 계산 Hibersine folmular 적용 및 정렬
+ * [buildDirectionListBytCategoryApi]
+ * 	- 카테고리 검색 API 활용으로 목록 추출 - `kakaoCategorySearchService.requestPharmacyCategorySearch()`
+ * 		- 위,경도 데이터를 기준 파라미터로 카카오 카테고리 장소검색 API를 통한 약국데이터 수집
+ * 		- 고객 약국사이 거리 정보 distance API로 부터 반환 및 정렬
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -27,6 +40,7 @@ public class DirectionService {
     private static final int MAX_SEARCH_COUNT = 3;// 약국 최대 검색 갯수
     private static final double RADIUS_KM = 10.0;// 반경 10 KM
     private final PharmacySearchService pharmacySearchService;
+    private final KakaoCategorySearchService kakaoCategorySearchService;
 
     /**
      * 최대 3개의 약국데이터 거리계산 목록 벌크 저장
@@ -36,7 +50,6 @@ public class DirectionService {
         if(CollectionUtils.isEmpty(directionList)) return Collections.emptyList(); // 목록이 비어있으면 저장처리하지 않고 빈 컬렉션 반환
         return directionRepository.saveAll(directionList);
     }
-
 
     /**
      * 최대 3개의 약국 데이터
@@ -79,6 +92,41 @@ public class DirectionService {
                 .limit(MAX_SEARCH_COUNT)
                 .collect(Collectors.toList());
     }
+
+    /**
+     * Kakao 카테고리별 장소검색 Api를 활용 <br/>
+     * 반경 10KM이내 가장 가까운 인근 약국 데이터 3개 목록 조회 <br/>
+     * 거리정보는 알고리즘 적용을 하지 않고 API에 의해 응답으로 받는다.
+     */
+    public List<Direction> buildDirectionListBytCategoryApi(DocumentDto inputDocumentDto) {
+        if (Objects.isNull(inputDocumentDto)) return Collections.emptyList();
+
+        /* Kakao 카테고리별 장소검색 Api - 약국 카테고리로 약국 목록 검색 */
+        List<DocumentDto> documentList =
+                kakaoCategorySearchService
+                        .requestPharmacyCategorySearch(inputDocumentDto.getLatitude(), inputDocumentDto.getLongitude(), RADIUS_KM)
+                        .getDocumentList();
+
+        return documentList.stream()
+                .map(resultDocumentDto ->
+                        Direction.builder()
+                                /* 고객이 입력한 정보(고객위치) */
+                                .inputAddress(inputDocumentDto.getAddressName())
+                                .inputLatitude(inputDocumentDto.getLatitude())
+                                .inputLongitude(inputDocumentDto.getLongitude())
+                                /* 약국의 정보 */
+                                .targetPharmacyName(resultDocumentDto.getPlaceName())
+                                .targetAddress(resultDocumentDto.getAddressName())
+                                .targetLatitude(resultDocumentDto.getLatitude())
+                                .targetLongitude(resultDocumentDto.getLongitude())
+                                /* 고객-약국간 거리 */
+                                .distance(resultDocumentDto.getDistance())
+                                .build()
+                )
+                .limit(MAX_SEARCH_COUNT)
+                .collect(Collectors.toList());
+    }
+
 
     /**
      * 두 위/경도 간의 거리를 구한다. <br/>
