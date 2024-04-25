fccf2094f895da9e486178b2d70438d876481216
webdevyoo <yjk7454@naver.com>
2024-04-26 00:02:31 +0900

    Ch.06-03.[검색-결과] 검색된 주소기반 약국 추천 결과조회 및 뷰  Post "/search" 핸들러 매핑 구현

--- src/main/java/com/recommend/pharmacy/domain/direction/controller/FormController.java
+++ src/main/java/com/recommend/pharmacy/domain/direction/controller/FormController.java
@@ -1,16 +1,31 @@
 package com.recommend.pharmacy.domain.direction.controller;
 
+import com.recommend.pharmacy.domain.direction.dto.InputDto;
+import com.recommend.pharmacy.domain.pharmacy.service.PharmacyRecommendationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
 public class FormController {
 
+    private final PharmacyRecommendationService pharmacyRecommendationService;
+
     @GetMapping("/")
     public String main() {
         return "main";
+    }
+
+    @PostMapping("/search")
+    public ModelAndView postDirection(@ModelAttribute InputDto inputDto) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("output");
+        modelAndView.addObject("outputFormList", pharmacyRecommendationService.recommendPharmacyList(inputDto.getAddress()));
+        return modelAndView;
     }
 }
