b82d394c14028977f7ff31ffe7d40a88847a8418
YooHyeok <yjk7454@naver.com>
2024-05-01 21:03:33 +0900

    Ch.08-03.[Redis 적용]: 약국 전체 조회 기능 수정 - Redis 조회 및 장애 발생시 기존 Mariad DB 조회

--- src/main/java/com/recommend/pharmacy/domain/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/recommend/pharmacy/domain/pharmacy/service/PharmacySearchService.java
@@ -1,5 +1,6 @@
 package com.recommend.pharmacy.domain.pharmacy.service;
 
+import com.recommend.pharmacy.domain.pharmacy.cache.PharmacyRedisTemplateService;
 import com.recommend.pharmacy.domain.pharmacy.dto.PharmacyDto;
 import com.recommend.pharmacy.domain.pharmacy.entity.Pharmacy;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +15,24 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PharmacySearchService {
     private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     /**
      * 약국 전체 조회 - DTO변환 및 반환
+     * Redis에 약국 데이터가 비어있지 않다면 Redis로부터 데이터를 반환하고,
+     * Redis에 데이터가 없으면 Maria DB에서 조회하여 반환한다.
+     * 데이터가 없으면 Redis에 장애가 발생한 상태임.
+     * (deserialize 도중 readValue에서 Exception발생)
      * @return
      */
     public List<PharmacyDto> searchPharmacyDtoList() {
 
         /* Redis */
+        List<PharmacyDto> pharmacyDtos = pharmacyRedisTemplateService.findAll();
+        if (!pharmacyDtos.isEmpty()) {
+            log.info("redis findAll success!");
+            return pharmacyDtos;
+        }
 
         /* MariaDB - 전체 조회 후 DTO변환하여 List로 반환 */
         return pharmacyRepositoryService.findAll()
